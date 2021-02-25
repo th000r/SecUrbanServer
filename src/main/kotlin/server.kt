eb526f577f6011c4116f77e96a8308561faca270
Roberts Marky <roberts.marky@web.de>
2021-02-25 10:24:43 +0100

    Switched from Tomcat to Netty

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -4,7 +4,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.tomcat.Tomcat
+import io.ktor.server.netty.*
 import kotlinx.html.*
 
 fun HTML.index() {
@@ -13,13 +13,13 @@ fun HTML.index() {
     }
     body {
         div {
-            +"Hello from Ktor"
+            +"Hello from Ktor with Netty"
         }
     }
 }
 
 fun main() {
-    embeddedServer(Tomcat, port = 8080, host = "127.0.0.1") {
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         routing {
             get("/") {
                 call.respondHtml(HttpStatusCode.OK, HTML::index)
