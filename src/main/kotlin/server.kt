8334198117d5ff96c504358ae5d3f388cf1d7e2b
Roberts Marky <roberts.marky@web.de>
2021-02-25 13:09:38 +0100

    Simple report saving and retrieving API.

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,11 +1,15 @@
-import io.ktor.application.call
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.html.respondHtml
 import io.ktor.http.HttpStatusCode
-import io.ktor.routing.get
-import io.ktor.routing.routing
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.*
 import kotlinx.html.*
+import java.text.DateFormat
 
 fun HTML.index() {
     head {
@@ -18,11 +22,40 @@ fun HTML.index() {
     }
 }
 
+data class Report(val citizenId: String, val message: String, val locationData: Boolean, val picture: Boolean)
+
+
+val reportStorage = mutableMapOf<String, Report>()
+
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+        install(ContentNegotiation) {
+            gson {
+                setDateFormat(DateFormat.LONG)
+                setPrettyPrinting()
+            }
+        }
         routing {
             get("/") {
                 call.respondHtml(HttpStatusCode.OK, HTML::index)
+            }
+            get("/report/{reportId}") {
+                val reportId = call.parameters["reportId"]
+                val report = reportStorage[reportId]
+                if (report != null) {
+                    call.respondText("$report", status = HttpStatusCode.OK)
+                } else {
+                    call.respondText("No report for id $reportId.", status = HttpStatusCode.NotFound)
+                }
+            }
+            get("/report") {
+                call.respondText("${reportStorage.keys}")
+            }
+            post("/report") {
+                val report = call.receive<Report>()
+                val reportId = Integer.toHexString(report.hashCode())
+                reportStorage[reportId] = report
+                call.respondText("Received report $report and saved with the id $reportId.", status = HttpStatusCode.OK)
             }
         }
     }.start(wait = true)
