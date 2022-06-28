470d4f459bab435030b6e46c42880ffb9a0d93e5
ThorstenSch <>
2022-06-28 19:03:56 +0200

    - updated Dockerfile - updated readme

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -2,6 +2,6 @@
 
 /usr/bin/mysqld_safe --skip-grant-tables &
 sleep 5
-mysql -u root -e "CREATE DATABASE persuasion_app"
-mysql -u root persuasion_app < ./sql/user_setup.sql
-mysql -u root persuasion_app < ./sql/db_setup.sql
+mysql -u root -p $1 -e "CREATE DATABASE persuasion_app"
+mysql -u root -p $1 persuasion_app < ./sql/user_setup.sql
+mysql -u root -p $1 persuasion_app < ./sql/db_setup.sql
