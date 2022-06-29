79cac7469bacbe936cf5895c112c95ba46524072
ThorstenSch <>
2022-06-29 12:31:26 +0200

    - updated Dockerfile - updated readme

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-/usr/bin/mysqld_safe --skip-grant-tables &
-sleep 5
-mysql -u root -p $1 -e "CREATE DATABASE persuasion_app"
-mysql -u root -p $1 persuasion_app < ./sql/user_setup.sql
-mysql -u root -p $1 persuasion_app < ./sql/db_setup.sql
+rm -rf ../mariadb-data
