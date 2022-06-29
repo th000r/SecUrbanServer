ae55a0f3cc54cfc8b716a8d840e272b8f6995be9
ThorstenSch <>
2022-06-29 12:09:06 +0200

    - updated Dockerfile - updated readme

--- sql/user_setup.sql
+++ sql/user_setup.sql
@@ -1,3 +1,3 @@
--- CREATE USER IF NOT EXISTS pa_root@'localhost' IDENTIFIED BY 'password';
+-- CREATE USER IF NOT EXISTS pa_root@'localhost' IDENTIFIED BY 'pw';
 GRANT ALL PRIVILEGES ON persuasion_app.* TO pa_root@'localhost';
 FLUSH PRIVILEGES;
