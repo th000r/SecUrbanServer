c419a1674ef0f1070edb2ccf9c0b3a84feddf00c
ThorstenSch <>
2022-06-29 17:39:04 +0200

    - updated config.json

---
+++ docker/matomo.Dockerfile
@@ -0,0 +1,2 @@
+# get base image
+FROM matomo
--- sql/matomo_setup.sql
+++ sql/matomo_setup.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS matomo_securban;
-CREATE USER IF NOT EXISTS 'matomo'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *. * TO matomo@'localhost';
+CREATE DATABASE IF NOT EXISTS persuasion_app_matomo;
+-- CREATE USER IF NOT EXISTS 'matomo_root'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON persuasion_app_matomo.* TO pa_root@'%';
 FLUSH PRIVILEGES;
-GRANT FILE ON *.* TO 'matomo'@'localhost';
+GRANT FILE ON persuasion_app_matomo.* TO 'pa_root'@'%';
